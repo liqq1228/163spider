d82f9632ea23b6ca7c76d03abcf7537b271539fb
leyle <leyle@leyle.com>
2015-03-10 11:27:42 +0800

    modify logging file

--- utils/mylogger.py
+++ utils/mylogger.py
@@ -3,53 +3,66 @@
 '''
 #=============================================================================
 # FileName:     mylogger.py
-# Desc:         日志记录函数，可以滚动打包日志
+# Desc:         python logger support zip log file
 # Author:       leyle
 # Email:        leyle@leyle.com
 # HomePage:     http://www.leyle.com/
 # Git_page:     https://github.com/leyle
-# Version:      0.0.1
-# LastChange:   2014-12-08 10:13:38
-# History:      
+# Version:      0.1.2
+# LastChange:   2015-03-10 11:09:59
 #=============================================================================
 '''
-
 import logging
 import logging.handlers
 import sys
 import os
-import time
 
 LOGGING_MSG_FORMAT = "%(name)s %(levelname)s %(asctime)s: %(message)s"
 LOGGING_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-MAXBYTE = 1024*1024*50  #50M
-BACKUPCOUNT = 20  #循环备份的最大数
 
-def get_logger(logname):
-    root_logger = logging.getLogger(logname)
+def get_logger(logfile, path="logs/", level=logging.DEBUG, max_byte=1024*1024*50, backup_count=10):
+    root_logger = logging.getLogger(logfile)
     if len(root_logger.handlers) == 0:
-        path = os.path.join(sys.path[0], 'logs/')
-        if not os.path.isdir(path):
-            os.mkdir(path)
-        filename = path + logname + ".log"
+        if path.startswith('/'):
+            if not os.path.isdir(path):
+                try:
+                    os.makedirs(path)
+                except OSError as e:
+                    print e
+                    sys.exit(1)
+            else:
+                if not os.access(path, os.R_OK|os.W_OK):
+                    print path, "without read/write permission"
+                    sys.exit(1)
+        else:
+            """ create new log file path, pwd+path """
+            path = os.path.join(sys.path[0], path)
+            if not path.endswith('/'):
+                path = path + '/'
+            if not os.path.isdir(path):
+                os.makedirs(path)
+
         handler = logging.handlers.RotatingFileHandler(
-                    filename,
+                    path + logfile + ".log",
                     mode = "a",
-                    maxBytes = MAXBYTE,
-                    backupCount = BACKUPCOUNT,
+                    maxBytes = max_byte,
+                    backupCount = backup_count,
                     encoding = "utf-8"
                     )
+
         fmter = logging.Formatter(LOGGING_MSG_FORMAT, LOGGING_DATE_FORMAT)
         handler.setFormatter(fmter)
         root_logger.addHandler(handler)
-        root_logger.setLevel(logging.DEBUG)
+        root_logger.setLevel(level)
 
-    line_name = "%s" % logname
-    return logging.getLogger(line_name)
+    return logging.getLogger(logfile)
+
+
 
 def test():
-    mylog = get_logger("log_name")
-    for i in range(0, 1000):
+    #get_logger(logfile, path="logs/", level=logging.DEBUG, max_byte=1024*1024*50, backup_count=10):
+    mylog = get_logger("log_name", "abc/def", max_byte=100)
+    for i in range(0, 10000):
         mylog.info("%d" % i)
 
 if __name__ == "__main__":
